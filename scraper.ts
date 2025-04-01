5aba54fda9ffb38145dedd5dc83e6d7d48dc0fbf
Urcodingbuddy <anshpethe110@gmail.com>
2025-04-01 17:46:43 +0530

    First commit

---
+++ scraper.ts
@@ -0,0 +1,39 @@
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from "puppeteer-extra-plugin-stealth"
+import { Browser } from 'puppeteer';
+// import chromium from '@sparticuz/chromium';
+puppeteer.use(StealthPlugin());
+
+const main = async () => {
+    try {
+        const browser: Browser = await puppeteer.launch({
+            headless:true
+        });
+        const page = await browser.newPage();
+        const BASE_URL = "https://bot.sannysoft.com/";
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+        await page.goto(BASE_URL, { waitUntil: "domcontentloaded" });
+        await page.screenshot({"path":"bot.jpeg"})
+        await browser.close()
+    } catch (error) {
+        console.error('An error occurred with Amazon scraping:', error);
+        return [];
+    }
+}
+main()
+
+/*
+{
+    args: [...chromium.args, 
+        "--no-sandbox", 
+        "--disable-setuid-sandbox", 
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--disable-gpu"],
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    defaultViewport: chromium.defaultViewport
+}
+*/
